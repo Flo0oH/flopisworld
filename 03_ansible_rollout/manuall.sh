52cefb170668057c0135c937486ff71f118dde4a
florian.gerstner <florian.gerstner@arcor.de>
2024-12-17 22:41:57 +0100

    manual work

---
+++ 03_ansible_rollout/manuall.sh
@@ -0,0 +1,11 @@
+ssh root@172.24.0.7   # Für pi2
+apt-get update && apt-get install -y python3
+exit
+
+ssh root@172.24.0.6   # Für pi3
+apt-get update && apt-get install -y python3
+exit
+
+ssh root@172.24.0.5   # Für pi4
+apt-get update && apt-get install -y python3
+exit
