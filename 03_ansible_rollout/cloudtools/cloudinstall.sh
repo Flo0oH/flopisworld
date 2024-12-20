5575298d62466ba27e8beef28e0f996736f00d1c
florian.gerstner <florian.gerstner@arcor.de>
2024-12-20 21:19:28 +0100

    n o up date

--- 03_ansible_rollout/cloudtools/cloudinstall.sh
+++ 03_ansible_rollout/cloudtools/cloudinstall.sh
@@ -29,7 +29,7 @@ if ! check_command docker; then
   apt-get install -y apt-transport-https ca-certificates curl software-properties-common
   curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add -
   add-apt-repository "deb [arch=arm64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
-  apt-get update
+ 
   apt-get install -y docker-ce docker-ce-cli containerd.io
   systemctl enable docker
   systemctl start docker
